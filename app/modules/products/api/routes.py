2a63d9d0418d8b329f51782f18d6b8faf96a860c
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-19 22:27:14 -0300

    🗃 add Supabase connection

--- app/modules/products/api/routes.py
+++ app/modules/products/api/routes.py
@@ -1,11 +1,13 @@
 # app/modules/products/api/routes.py
 from fastapi import APIRouter
+from app.services.supabase_client import supabase
 
 router = APIRouter()
 
 @router.get("/")
-def get_users():
-    return [{"id": 1, "brand": "Toyota"}, {"id": 2, "brand": "Peugeot"}]
+def get_products():
+    response = supabase.table("products").select("*").execute()
+    return response.data
 
 @router.post("/")
 def create_user(product: dict):
