13b45c5ebe13061262f08105d5957fbf34f39d6d
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-22 22:05:30 -0300

    🔧 refact supabase_client

--- app/services/supabase_client.py
+++ app/services/supabase_client.py
@@ -2,7 +2,7 @@
 from supabase import create_client, Client
 from app.config.settings import settings
 
-supabase: Client = create_client(
-    settings.SUPABASE_URL,
-    settings.SUPABASE_KEY
-)
+url: str = settings.SUPABASE_URL
+key: str = settings.SUPABASE_KEY
+
+supabase: Client = create_client(url, key)
