ed1c3544a7836c117c173cbbcd8d4acef05dec6f
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-26 01:09:57 -0300

    ✨ apply logging in main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from typing import Union
 from fastapi import FastAPI
+
+from app.config.logging import setup_logging
 from app.middleware.error_handler import custom_error_handler
 from app.modules.routes import register_routes
+
+setup_logging()
 
 app = FastAPI(title="DealerApp API")
 
