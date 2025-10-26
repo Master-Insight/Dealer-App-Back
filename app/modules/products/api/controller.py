c7da9970a43e7ba26bcc31dafbb6c324bb09dd1b
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-26 00:22:23 -0300

    🔼 improve product/controller

--- app/modules/products/api/controller.py
+++ app/modules/products/api/controller.py
@@ -11,6 +11,7 @@ class ProductController(ResponseController[Product, ProductCreate]):
     def __init__(self):
         super().__init__(ProductService())
 
-    def create(self, data):
-        """Crea un nuevo registro."""
-        return ResponseBuilder.success("creado")
+    def create(self, data: ProductCreate):
+        """Crea un nuevo producto en la base de datos."""
+        created_product = self.service.create(data.dict())
+        return ResponseBuilder.success(created_product, "Producto creado correctamente")
