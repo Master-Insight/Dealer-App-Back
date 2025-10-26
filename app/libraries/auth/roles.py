4775fb535c281b37af0ea85b0935bda2b97747c8
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-26 19:49:43 -0300

    🔧 fix role check

--- app/libraries/auth/roles.py
+++ app/libraries/auth/roles.py
@@ -8,9 +8,25 @@ from app.modules.users.logic.services import UserService
 def require_role(allowed_roles: list[str]):
     async def role_checker(current_user=Depends(get_current_user)):
         user_service = UserService()
+
         profile = user_service.get_user_by_email(current_user.email)
-        if not profile or profile["role"] not in allowed_roles:
-            raise AuthError("No tienes permiso")
+
+        if not profile:
+            raise AuthError("Perfil no encontrado para este usuario")
+
+        role = (
+            profile.get("role")
+            if isinstance(profile, dict)
+            else getattr(profile, "role", None)
+        )
+
+        # print("\nPerfil encontrado:", profile)
+        # print("Rol del usuario:", role)
+        # print("Roles permitidos:", allowed_roles)
+
+        if role not in allowed_roles:
+            raise AuthError("No tienes permiso para acceder a este recurso")
+
         return profile
 
     return role_checker
