7507fe950ca4e339abf194839e4e27fd1f2b1e36
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-29 00:11:11 -0300

    ➕ add middleware request_context.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.config.logging import setup_logging
 from app.middleware.error_handler import custom_error_handler
 from app.modules.routes import register_routes
+from app.middleware.request_context import RequestContextLogMiddleware
 
 setup_logging()
 
@@ -10,6 +11,7 @@ app = FastAPI(title="DealerApp API")
 
 # Middlewares
 app.middleware("http")(custom_error_handler)
+app.add_middleware(RequestContextLogMiddleware)
 
 # Registrar todas las rutas de los módulos
 register_routes(app)
