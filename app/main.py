f63548ff502a11cdd400d0dfd45795e5c5b8baf6
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-11-01 01:30:13 -0300

    🔧 add CORS policies

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config.logging import setup_logging
 from app.middleware.error_handler import custom_error_handler
@@ -8,6 +9,19 @@ from app.middleware.request_context import RequestContextLogMiddleware
 setup_logging()
 
 app = FastAPI(title="DealerApp API")
+
+# 🔹 Lista de orígenes permitidos
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite GET, POST, PUT, DELETE, OPTIONS, etc.
+    allow_headers=["*"],  # Permite headers personalizados como Authorization
+)
 
 # Middlewares
 app.middleware("http")(custom_error_handler)
